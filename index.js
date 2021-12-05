61f15c8234dfa672070cf914b1a03be4d45700e6
Erik Luebbers <erluebbers@gmail.com>
2021-12-05 09:39:54 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string) {
+    const excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const splitString = string.split(" ")
+    for (const word of splitString) {
+      cap(word)
+    }
+    console.log(splitString)
+    return splitString.join(" ")
+  }
 }
+
+function cap (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+// excludeWords.includes(word) ? word : cap(word)
